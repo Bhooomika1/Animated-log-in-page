09422f835583e9dbdc32f349243c75c53afeeedf
Bhooomika1 <bhooomikapal10@gmail.com>
2024-11-21 00:08:27 +0530

    updates

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const loginButton = document.getElementById("login");
-const registerButton = document.getElementById("register");
-const container = document.getElementById("container");
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
 
-loginButton.addEventListener("click", () => {
-    container.classList.remove("right-panel-active");
+signUpButton.addEventListener('click', () => {
+    container.classList.add('right-panel-active');
 });
 
-registerButton.addEventListener("click", () => {
-    container.classList.add("right-panel-active");
-});   
+signInButton.addEventListener('click', () => {
+    container.classList.remove('right-panel-active');
+});
